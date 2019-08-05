fe7999e3050718d09103c3c8f1a9243cf709f088
Lance Smith <lance.matthew.smith@gmail.com>
2019-08-04 17:29:55 -0700

    Solve isMatch regex problem

--- isMatch.js
+++ isMatch.js
@@ -14,56 +14,26 @@
 
 */
 
-// need to handle starmode w/ wildcard
-
-const isMatch = (str, pattern) => {
-    let i;
-    let j = 0;
-    for (i = 0; i < pattern.length;) {
-        let starMode = false;
-        let wildcard = false;
-        let repeatAfterStar = false;
-
-        if (pattern[i + 1] === '*') {
-            starMode = true; // check next char for *
-            if (pattern[i + 2] === pattern[i]) repeatAfterStar = true;
+const isMatch = (s, p) => {
+    if (!p.length) {
+        if (!s.length) {
+            return true;
         }
-        if (pattern[i] === '.') wildcard = true; // check if current char is .
-
-        if (!starMode) {
-            if (pattern[i] === str[j]) { // current char matches pattern char
-                i++;
-                j++;
-            } else if (wildcard) {
-                i++;
-                j++;
-            } else { // return false early if mismatch is discovered
-                console.log("mismatch found returning false early");
-                return false;
-            }
-        } else { // currently in starmode
-            if (!wildcard) {
-                while (pattern[i] === str[j]) { // at least one matching char is found
-                    j++; // move to next character in the string
-                }
-            } else {
-                while (str[j]) {
-                    j++;
-                }
-            }
-
-            i += 2; // move past * in pattern
-            if (repeatAfterStar) i++;
+        else {
+            return false;
         }
     }
 
-    if ((i === pattern.length) && (j === str.length)) {
-        return true;
+    let firstMatch = ((s.length > 0) && (p[0] === s[0] || p[0] === '.'));
+
+    if (p.length >= 2 && p[1] == '*') {
+        return (isMatch(s, p.slice(2)) || (firstMatch && isMatch(s.slice(1), p)));
     } else {
-        return false;
+        return firstMatch && isMatch(s.slice(1), p.slice(1));
     }
 };
 
+console.log(isMatch('aa', 'a')); // false
 console.log(isMatch('aab', 'c*a*b')); // true
 console.log(isMatch('aaa', 'a*a')); // true
 console.log(isMatch('aaa', 'ab*a*c*a')); // should be true
