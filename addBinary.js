b69774cef4e876823a574b98b29b9eb1dc30fdda
Lance Smith <lance.matthew.smith@gmail.com>
2019-11-09 18:16:46 -0800

    Add Binary problem

---
+++ addBinary.js
@@ -0,0 +1,21 @@
+const addBinary = function(a, b) {
+    let sum = '';
+
+
+    return sum;
+};
+
+// 3 + 1 = 4
+console.log(addBinary('11', '1')); // 100
+
+// 10 + 11 = 21
+console.log(addBinary('1010', '1011')); // 10101
+
+/*
+
+11 1
+1 + 1 = 2, 0 carry 1
+1 + 1 = 2, 0 carry 1
+1 = 1
+
+*/
